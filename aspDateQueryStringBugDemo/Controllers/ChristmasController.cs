a1c1c506ba3bd5e8d333368109aec383f8b2dd5e
Wayne Atkinson Young <youngwt@gmail.com>
2021-10-20 21:31:35 +0100

    Sample project added

---
+++ aspDateQueryStringBugDemo/Controllers/ChristmasController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace aspDateQueryStringBugDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChristmasController : ControllerBase
+    {
+        private readonly ILogger<ChristmasController> _logger;
+
+        public ChristmasController(ILogger<ChristmasController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult IsItChristmas(DateTime date)
+        {
+            if(date.Month == 12 && date.Day == 25)
+            {
+                return Ok("Yes!");
+            }
+
+            return Ok("No!");
+        }
+    }
+}
